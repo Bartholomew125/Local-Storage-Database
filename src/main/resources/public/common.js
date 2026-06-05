4ee1cdb318e0196a47e50c990b17059e2b6a8be8
Andreas Kapp Lindquist <andreas@Andreass-MacBook-Air.local>
2026-06-05 14:36:05 +0200

    Feat: added navbar and home screen

---
+++ src/main/resources/public/common.js
@@ -0,0 +1,7 @@
+// In common.js, loaded by every page
+async function loadComponent(id, url) {
+    const res = await fetch(url);
+    document.getElementById(id).innerHTML = await res.text();
+}
+
+loadComponent("navbar", "/components/navbar.html");
